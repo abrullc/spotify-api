2a9a4a1f79e34f3dc355e8894ec316bfe77fe219
abrullc <adrianbrullcontreras@gmail.com>
2024-01-22 12:36:37 +0100

    UsuarioController acabado

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -17,8 +17,8 @@ class UsuarioController extends AbstractController
     {
         if ($request->isMethod("GET")) {
             $usuarios = $this->getDoctrine()
-            ->getRepository(Usuario::class)
-            ->findAll();
+                ->getRepository(Usuario::class)
+                ->findAll();
 
             $usuarios = $serializer->serialize(
                 $usuarios,
@@ -46,6 +46,67 @@ class UsuarioController extends AbstractController
                 ["groups" => ["usuario", "cancion", "podcast", "album", "artista", "playlist"]]);
             
             return new Response($usuario);
+        }
+
+        return new JsonResponse(["msg" => $request->getMethod() . " no permitido"]);
+    }
+
+    public function usuario(SerializerInterface $serializer, Request $request)
+    {
+        $id = $request->get("id");
+
+            $usuario = $this->getDoctrine()
+                ->getRepository(Usuario::class)
+                ->findOneBy(["id" => $id]);
+
+        if ($request->isMethod("GET")) {
+
+            $usuario = $serializer->serialize(
+                    $usuario,
+                    "json",
+                    ["groups" => ["usuario", "cancion", "podcast", "album", "artista", "playlist"]]
+                );
+
+            return new Response($usuario);
+        }
+
+        if ($request->isMethod("PUT")) {
+            if (!empty($usuario)) {
+                $bodyData = $request->getContent();
+                $usuario = $serializer->deserialize(
+                    $bodyData,
+                    Usuario::class,
+                    "json",
+                    ["object_to_populate" => $usuario]
+                );
+                
+                $this->getDoctrine()->getManager()->persist($usuario);
+                $this->getDoctrine()->getManager()->flush();
+
+                $usuario = $serializer->serialize(
+                    $usuario,
+                    "json",
+                    ["groups" => ["usuario", "cancion", "podcast", "album", "artista", "playlist"]]
+                );
+
+            return new Response($usuario);
+            }
+
+            return new JsonResponse(["msg" => "Affiliation no encontrada"], 404);
+        }
+
+        if ($request->isMethod("DELETE")) {
+            $deletedUsuario = clone $usuario;
+            $this->getDoctrine()->getManager()->remove($usuario);
+            $this->getDoctrine()->getManager()->flush();
+            
+            $deletedUsuario = $serializer->serialize(
+                $deletedUsuario, 
+                "json", 
+                ["groups" => ["usuario", "cancion", "podcast", "album", "artista", "playlist"]]
+            );
+
+            return new Response($deletedUsuario);
         }
 
         return new JsonResponse(["msg" => $request->getMethod() . " no permitido"]);
