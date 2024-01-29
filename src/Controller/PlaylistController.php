927cd230db69b650e2d9f91dd1a409c313d09b18
Julio <badthunder115@gmail.com>
2024-01-29 18:57:01 +0100

    Playlist terminado

--- src/Controller/PlaylistController.php
+++ src/Controller/PlaylistController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 # Fichero INcompleto
 
 use App\Entity\Playlist;
+use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,7 +13,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class PlaylistController extends AbstractController
 {
-    public function playlists(SerializerInterface $serializer, Request $request)
+    public function playlists(SerializerInterface $serializer)
     {
         $playlists = $this->getDoctrine()
             ->getRepository(Playlist::class)
@@ -20,9 +21,90 @@ class PlaylistController extends AbstractController
 
         $playlists = $serializer->serialize(
             $playlists,
-                "json",
-                ["groups" => ["plylist"]]
-            );
+            "json",
+            ["groups" => ["playlist"]]
+        );
 
         return new Response($playlists);
-        }}
+    }
+    public function playlist(SerializerInterface $serializer, Request $request)
+    {
+        $id = $request->get("id");
+        $playlist = $this->getDoctrine()
+            ->getRepository(Playlist::class)
+            ->findOneBy(["id" => $id]);
+
+        $playlist = $serializer->serialize(
+            $playlist,
+            "json",
+            ["groups" => ["playlist"]]
+        );
+        return new Response($playlist);
+    }
+    public function playlistsUsuario(SerializerInterface $serializer, Request $request)
+    {
+        $id = $request->get("usuario");
+        $usuario = $this->getDoctrine()
+            ->getRepository(Usuario::class)
+            ->findOneBy(["id" => $id]);
+        if ($request->isMethod("GET")) {
+
+            $playlists = $this->getDoctrine()
+            ->getRepository(Playlist::class)
+            ->findBy([$usuario->getId() => ["id"]]);
+
+            $playlists = $serializer->serialize(
+                $playlists,
+                "json",
+                ["groups" => ["playlist"]]
+            );
+            return new Response($playlists);
+        }
+        if ($request->isMethod("POST")) {
+            $bodyData = $request->getContent();
+            $playlist = $serializer->deserialize(
+                $bodyData,
+                Playlist::class,
+                "json"
+            );
+            $playlist->setUsuario($usuario);
+            $this->getDoctrine()->getManager()->persist($playlist);
+            $this->getDoctrine()->getManager()->flush();
+
+            $playlist = $serializer->serialize(
+                $playlist, 
+                "json", 
+                ["groups" => ["playlist"]]);
+            
+            return new Response($playlist);
+        }
+    }
+    public function playlistUsuario(SerializerInterface $serializer, Request $request)
+    {
+        $id1 = $request->get("playlistId");
+        $id2 = $request->get("usuario");
+        $playlist = $this->getDoctrine()
+            ->getRepository(Playlist::class)
+            ->findOneBy(["id" => $id1]);
+
+        $usuario = $this->getDoctrine()
+            ->getRepository(Usuario::class)
+            ->findOneBy(["id" => $id2]);
+        if ($request->isMethod("GET")) {
+
+            if ($usuario->getId() == $playlist->getUsuario()->getId()) {
+
+                $playlist = $serializer->serialize(
+                    $playlist,
+                    "json",
+                    ["groups" => ["playlist"]]
+                );
+            }
+            return new Response($playlist);
+        }
+        if ($request->isMethod("POST")) {
+        }
+        if ($request->isMethod("GET")) {
+        }
+    }
+}
