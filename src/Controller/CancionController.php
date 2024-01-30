5701cdf47900bf0a76f101fe95ef2cd8d85f524a
abrullc <adrianbrullcontreras@gmail.com>
2024-01-30 13:07:18 +0100

    Routes de CANCIONES corregidas. CancionController a falta de terminar de implementar cancionesPlaylist y añadir cancionPlaylist

--- src/Controller/CancionController.php
+++ src/Controller/CancionController.php
@@ -2,13 +2,85 @@
 
 namespace App\Controller;
 
+# Fichero de Adrián
+
+use App\Entity\Cancion;
+use App\Entity\Playlist;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CancionController extends AbstractController
 {
-    public function index()
+    public function canciones(SerializerInterface $serializer, Request $request)
     {
-        return new Response('Hello!');
+        if ($request->isMethod("GET")) {
+            $canciones = $this->getDoctrine()
+                ->getRepository(Cancion::class)
+                ->findAll();
+
+            $canciones = $serializer->serialize(
+                $canciones,
+                "json",
+                ["groups" => ["cancion", "album", "artista"]]
+            );
+
+            return new Response($canciones);
+        }
+
+        return new JsonResponse(["msg" => $request->getMethod() . " no permitido"]);
+    }
+
+    public function cancion(SerializerInterface $serializer, Request $request)
+    {
+        $id = $request->get("id");
+
+        if ($request->isMethod("GET")) {
+            $cancion = $this->getDoctrine()
+                ->getRepository(Cancion::class)
+                ->findOneBy(["id" => $id]);
+
+            $cancion = $serializer->serialize(
+                $cancion,
+                "json",
+                ["groups" => ["cancion", "album", "artista"]]
+            );
+
+            return new Response($cancion);
+        }
+
+        return new JsonResponse(["msg" => $request->getMethod() . " no permitido"]);
+    }
+
+    public function cancionesPlaylist(SerializerInterface $serializer, Request $request)
+    {
+        $id = $request->get("id");
+
+        if ($request->isMethod("GET")) {
+            $playlist = $this->getDoctrine()
+                ->getRepository(Playlist::class)
+                ->findOneBy(["id" => $id]);
+
+            $cancionesPlaylist = $this->getDoctrine()
+                ->getRepository(Cancion::class)
+                ->findAll();
+
+            if (!empty($playlist))
+            {
+                $cancionesPlaylist = $serializer->serialize(
+                    $cancionesPlaylist,
+                    "json",
+                    ["groups" => ["cancion", "album", "artista"]]
+                );
+
+                return new Response($cancionesPlaylist);
+            }
+
+            return new JsonResponse(["msg" => "Playlist no encontrada"], 404);
+        }
+
+        return new JsonResponse(["msg" => $request->getMethod() . " no permitido"]);
     }
 }
